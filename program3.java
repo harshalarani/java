eafcf7260c3a16c6927ae767719eb1133addb48a
harshalarani <109454355+harshalarani@users.noreply.github.com>
2023-02-03 08:05:17 +0530

    Update program3.java

--- program3.java
+++ program3.java
@@ -1,44 +1,55 @@
 import java.util.*;
+
 class book{
-String name,author;
-int price,num_pages;
-  book(){
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter book name");
-name=sc.nextLine();
-System.out.println("Enter author name");
-author=sc.nextLine();
-System.out.println("Enter price");
-price=sc.nextInt();
-System.out.println("Enter number of pages");
-num_pages=sc.nextInt();
+String name;
+String author;
+double price;
+int pages;
+Scanner sc= new Scanner(System.in);
+book(){
+name="";
+author="";
+price=0.0;
+pages=0;
 }
-void display(){
-System.out.println("The details of the book are as follows");
-System.out.println("Book-Name : " + name+"\n");
-System.out.println("Book-Author : " + author+"\n");
-System.out.println("Book-Price : " + price+"\n");
-System.out.println("Book-Pages: " + num_pages+"\n");
-System.out.println("");
-}
-}
- 
 
-class week3{
+void set(){
+System.out.println("Enter name of the book");
+name=sc.next();
+System.out.println("Enter name of the author");
+author=sc.next();
+System.out.println("Enter price of the book");
+price=sc.nextDouble();
+System.out.println("Enter price of the book");
+pages=sc.nextInt();
+}
+
+
+//tostring() is an inbuilt funtion in java and helps us to print values with much ease
+//you dont have to call it when you use println it automatically goes there
+
+public String toString()
+{
+String s="Name of the book is "+name+" and it is written by "+author+". The book costs "+price+" and has "+ pages+" pages";
+return s;
+}
+}
+
+
+class bookmain{
 public static void main(String args[]){
-    Scanner sc= new Scanner(System.in);
- int n;
-System.out.println("Enter the number of books ");
+Scanner sc= new Scanner(System.in);
+System.out.println("Enter the number of books");
+int n;
 n=sc.nextInt();
-book b[]=new book[n];
+book b[]=new book[n]; //this is how you create an array of class
 for(int i=0;i<n;i++){
+System.out.println("Enter details of book "+(i+1));
 b[i]=new book();
+b[i].set();
 }
-
 for(int i=0;i<n;i++){
-
-b[i].display();
+System.out.println(b[i]);
 }
 }
 }
-
